aa62289cdfd535e2a0bc5b5ad68f4bd321ebba9b
yedp <ydpup@126.com>
2021-02-02 11:25:51 +0800

    初始化系统

---
+++ src/main/java/com/github/yedp/ez/cache/starter/EzCacheStarterApplication.java
@@ -0,0 +1,13 @@
+package com.github.yedp.ez.cache.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzCacheStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzCacheStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/yedp/ez/cache/starter/EzCacheStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.yedp.ez.cache.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzCacheStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
